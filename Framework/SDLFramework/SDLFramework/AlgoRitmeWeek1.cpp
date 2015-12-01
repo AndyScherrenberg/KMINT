37d66cf330ea76321c9bd2b0fd5959142117c3a6
AndyScherrenberg <AndyScherrenberg@Gmail.com>
2015-12-01 15:42:25 +0100

    Kleine fix en kans berekening

--- Framework/SDLFramework/SDLFramework/AlgoRitmeWeek1.cpp
+++ Framework/SDLFramework/SDLFramework/AlgoRitmeWeek1.cpp
@@ -92,7 +92,7 @@ void AlgoRitmeWeek1::aStar(Beestje* beestje, std::vector<Node*> collection, Node
             {
                 finder = destination;
             }
-            while(finder->traveledFrom != beestje->getNode()){
+                while(finder->traveledFrom != beestje->getNode()){
                 finder = finder->traveledFrom;
             }
             setNewCawPlace(finder, beestje);
